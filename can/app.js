6e40b5095373b6bca1a089b018aa9ae9d6a64092
Arthur Carabott <arthurc@gmail.com>
2017-02-23 13:04:47 +0000

    show number

--- can/app.js
+++ can/app.js
@@ -43,4 +43,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(require('connect-livereload')({
+  port: 35729
+}));
+
 module.exports = app;
--- can/routes/index.js
+++ can/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: '50 ways to change a number',
+    number: 666
+  });
 });
 
 module.exports = router;
